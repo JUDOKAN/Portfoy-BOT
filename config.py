50780da10349fcc65faec864d93eb9d7aafb72af
Mehmet Kalaycı <152727744+JUDOKAN@users.noreply.github.com>
2025-04-02 16:12:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
+#İstediğn bir isimi 3. satırdaki tırnak içine örn:xxx.db
+
 DATABASE = "your db name"
+
+#Discord developer portala git ve token al 7. satırdaki tırnak içine yaz
+
 TOKEN = "your token"
